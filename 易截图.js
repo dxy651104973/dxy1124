673a35202a7418e990b557bd9e5ee1d0b8eacf78
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-10 23:09:29 +0800

    易截图.js

--- 易截图.js
+++ 易截图.js
@@ -3,7 +3,7 @@
 易截图2：https://apps.apple.com/app/id1633186528
 
 [rewrite_local]
-^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/yijietu2.js
+^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body
 
 [MITM]
 hostname = jietu6.com, 1jietu.com
@@ -12,6 +12,5 @@ hostname = jietu6.com, 1jietu.com
 var guding = JSON.parse($response.body);
 guding.info.group_id = "3";
 guding.info.vip = "1";
-guding.info.group = "无耻的破解会员";
-guding.info.nickname = "骨钉 https://t.me/Guding88";
+guding.info.group = 创世会员";
 $done({ body: JSON.stringify(guding) });
