7078d941713fd77ceb79d2429dc48746853665c5
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-28 00:03:09 +0800

    Update yijietu2.js

--- yijietu2.js
+++ yijietu2.js
@@ -1,6 +1,14 @@
-var dxy = JSON.parse($response.body);
-dxy.info.group_id = "3";
-dxy.info.vip = "1";
-dxy.info.group = "永久会员";
-dxy.info.nickname = "呆萌可爱小奶0";
 $done({ body: JSON.stringify(dxy) });
+
+var objc = JSON.parse($response.body);
+
+if (objc.hasOwnProperty('info.group')) {
+    // 修改 data_gfen 的值为 "9999999"
+    objc.info.group = "永久会员";
+}
+
+objc.info.group_id = "3";
+objc.info.vip = "1";
+objc.info.nickname = "呆萌可爱小奶0";
+
+$done({body: JSON.stringify(objc)});
