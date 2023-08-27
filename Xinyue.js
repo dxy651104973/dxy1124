008213d53e8448c382c2eee21808f6339ab9a791
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-27 23:38:25 +0800

    Update Xinyue.js

--- Xinyue.js
+++ Xinyue.js
@@ -1,7 +1,7 @@
 var dxy = JSON.parse($response.body);
 
-if (dxy.hasOwnProperty（'gfen')) {
+if (dxy.hasOwnProperty('gfen')) {
   //修改gfen的值为999999 
-dxy.gfen = “999999”;
+dxy.gfen = “999999";
 }
 $done({ body: JSON.stringify(dxy) });
