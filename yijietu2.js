d373e3f6b534fecaa5845fa278433b2246c3f7f2
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-28 00:31:01 +0800

    Update yijietu2.js

--- yijietu2.js
+++ yijietu2.js
@@ -3,6 +3,6 @@ var dxy = JSON.parse($response.body);
 dxy.info.group = "永久会员";
 dxy.info.group_id = "3";
 dxy.info.vip = "1";
-dxy.info.nickname = "呆萌可爱小奶0";
+dxy.info.status = "呆萌可爱小奶0";
 
 $done({body: JSON.stringify(dxy)});
