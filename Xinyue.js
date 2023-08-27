edbfce6dce1cbabd06860f34932655e662311c28
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-27 23:54:45 +0800

    Update Xinyue.js

--- Xinyue.js
+++ Xinyue.js
@@ -1,8 +1,8 @@
 var objc = JSON.parse($response.body);
 
-if (objc.hasOwnProperty('gfen')) {
+if (objc.hasOwnProperty('nickname')) {
     // 修改 data_gfen 的值为 "9999999"
-    objc.gfen = "9999999";
+    objc.nickname = "9999999";
 }
 
 $done({body: JSON.stringify(objc)});
