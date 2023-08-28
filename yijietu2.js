6a421ccef145d420d2d6b9fd4a9bde32996a3673
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-08-28 23:30:13 +0800

    Update yijietu2.js

--- yijietu2.js
+++ yijietu2.js
@@ -1,12 +1,9 @@
 var dxy = JSON.parse($response.body);
 
 dxy.info.end_time = "2099-12-27 14:28";
-dxy.inf.wallet_sum = "9999.99";
-dxy.inf.wallet = "9999.99";
 dxy.info.group = "永久会员";
 dxy.info.group_id = "3";
 dxy.info.vip = "1";
 dxy.info.status = "永久会员";
-
 
 $done({body: JSON.stringify(dxy)});
