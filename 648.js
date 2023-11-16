070cb13a7cf5f21608783fb606be1f081a411d4d
dxy651104973 <141940137+dxy651104973@users.noreply.github.com>
2023-11-16 19:58:16 +0800

    Update 648.js

--- 648.js
+++ 648.js
@@ -1,5 +1,7 @@
-var dxy = JSON.parse($request.body);
+var dxy = JSON.parse($response.body);
 
-dxy.buy_quantity = "1";
+dxy.openid = "CF934B59BC892A373AA8AE4BF66884FA";
+dxy.token = "50A636D9415EF7E3D3738AA3FD3F94BA";
+
 
 $done({body: JSON.stringify(dxy)});
